063a5589d5c7786f3ae9aba57459b9a138deb1b7
mino-su <omsalstn0401@gmail.com>
2026-02-12 15:37:51 +0900

    Feat: QueryDsl BooleanExpression을 활용한 동적 검색 쿼리 구현

--- backend_core/src/main/java/com/project/plant_parent/repository/PostRepositoryCustom.java
+++ backend_core/src/main/java/com/project/plant_parent/repository/PostRepositoryCustom.java
@@ -1,11 +1,14 @@
 package com.project.plant_parent.repository;
 
 import com.project.plant_parent.entity.Post;
+import com.project.plant_parent.entity.dto.PostSearchConditionDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 
 // Querydsl 사용을 위한 인터페이스
 public interface PostRepositoryCustom {
-    Page<Post> findAllWithPaging(Pageable pageable);
+    Page<Post> findAllWithPaging(Pageable pageable); // 페이징
+
+    Page<Post> search(PostSearchConditionDto postSearchConditionDto, Pageable pageable);
 }
--- backend_core/src/main/java/com/project/plant_parent/repository/PostRepositoryCustomImpl.java
+++ backend_core/src/main/java/com/project/plant_parent/repository/PostRepositoryCustomImpl.java
@@ -1,14 +1,18 @@
 package com.project.plant_parent.repository;
 
-import com.project.plant_parent.entity.Post;
-import com.project.plant_parent.entity.QMember;
-import com.project.plant_parent.entity.QPost;
+import com.project.plant_parent.entity.*;
+import com.project.plant_parent.entity.dto.PostSearchConditionDto;
+import com.project.plant_parent.exception.BusinessException;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import io.netty.util.internal.StringUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.util.StringUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 // Querydsl 구현체
@@ -26,6 +30,7 @@ public class PostRepositoryCustomImpl implements PostRepositoryCustom{
         List<Post> content = queryFactory
                 .selectFrom(post)
                 .join(post.member, member).fetchJoin()
+                .leftJoin(post.postImages)
                 .orderBy(post.createdAt.desc())
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
@@ -42,4 +47,87 @@ public class PostRepositoryCustomImpl implements PostRepositoryCustom{
 
     }
 
+    @Override
+    public Page<Post> search(PostSearchConditionDto postSearchConditionDto, Pageable pageable) {
+        QPost post = QPost.post;
+        QMember member = QMember.member;
+        QPostImage postImage = QPostImage.postImage;
+
+        QPlantDictionary plantDictionary = QPlantDictionary.plantDictionary;
+        QDiseaseDictionary diseaseDictionary = QDiseaseDictionary.diseaseDictionary;
+
+        String type = postSearchConditionDto.getType(); // title, writer, content, plant, disease, all
+        String keyword = postSearchConditionDto.getKeyword();
+
+        List<Post> content = queryFactory
+                .selectFrom(post)
+                .join(post.member, member).fetchJoin()
+                .leftJoin(post.postImages, postImage)
+                .leftJoin(plantDictionary).on(postImage.plant.eq(plantDictionary.label))
+                .leftJoin(diseaseDictionary).on(postImage.disease.eq(diseaseDictionary.label))
+                .where(
+                        allSearchCond(type, keyword, plantDictionary, diseaseDictionary)
+                )
+                .orderBy(post.createdAt.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        Long total = queryFactory
+                .select(post.count())
+                .from(post)
+                .where(
+                        allSearchCond(type, keyword,plantDictionary,diseaseDictionary)
+                )
+                .fetchOne();
+
+        return new PageImpl<>(content, pageable, total != null  ? total : 0L);
+
+
+    }
+
+    // type에 따라 적절한 Boolean Expression 반환
+    private BooleanExpression allSearchCond(String type, String keyword,
+                                            QPlantDictionary plantDict, QDiseaseDictionary diseaseDict) {
+        // 검색어가 없으면 조건을 아예 걸지 않음
+        if(!StringUtils.hasText(keyword)) return null;
+
+        if("title".equals(type)) return postTitleContains(keyword);
+        if("writer".equals(type)) return writerNameContains(keyword);
+        if("content".equals(type)) return contentContains(keyword);
+        if("plant".equals(type)) return plantNameContains(keyword, plantDict);
+        if("disease".equals(type)) return diseaseNameContains(keyword,diseaseDict);
+
+        if("all".equals(type)){
+            return postTitleContains(keyword)
+                    .or(writerNameContains(keyword))
+                    .or(contentContains(keyword))
+                    .or(plantNameContains(keyword, plantDict))
+                    .or(diseaseNameContains(keyword, diseaseDict));
+        }
+
+        return null;
+    }
+
+    private BooleanExpression postTitleContains(String keyword) {
+        return QPost.post.title.containsIgnoreCase(keyword);
+    }
+
+    private BooleanExpression writerNameContains(String keyword) {
+        return QPost.post.member.username.containsIgnoreCase(keyword);
+    }
+
+    private BooleanExpression contentContains(String keyword) {
+        return QPost.post.content.containsIgnoreCase(keyword);
+    }
+
+    private BooleanExpression plantNameContains(String keyword, QPlantDictionary plantDict) {
+        return QPostImage.postImage.plant.containsIgnoreCase(keyword)
+                .or(plantDict.nameKr.contains(keyword));
+    }
+
+    private BooleanExpression diseaseNameContains(String keyword, QDiseaseDictionary diseaseDict) {
+        return QPostImage.postImage.disease.containsIgnoreCase(keyword)
+                .or(diseaseDict.nameKr.contains(keyword));
+    }
 }
