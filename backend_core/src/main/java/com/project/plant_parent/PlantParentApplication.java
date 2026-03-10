3240aa21cccca63d8a67c4b9b492d1192089c1a7
mino-su <omsalstn0401@gmail.com>
2026-03-10 17:00:28 +0900

    Refactor: TimeZone을 서울 시간으로 변경

--- backend_core/src/main/java/com/project/plant_parent/PlantParentApplication.java
+++ backend_core/src/main/java/com/project/plant_parent/PlantParentApplication.java
@@ -1,13 +1,20 @@
 package com.project.plant_parent;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.TimeZone;
 
 
 @SpringBootApplication
 public class PlantParentApplication {
 
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(PlantParentApplication.class, args);
 	}
